5e47feda825a027840e98ca491736637d55ff482
Jesse F. LaRue <lazaruslarue@gmail.com>
2014-06-25 11:32:55 -0700

    fixup! cleanup

--- www/js/states/home.js
+++ www/js/states/home.js
@@ -2,6 +2,6 @@ angular.module('states.home',[]).config( function ($stateProvider) {
   $stateProvider.state("home", {
     url: "/home",
     controller: "HomeCtrl",
-    templateUrl: "../templates/templates/home.html",
+    templateUrl: "../templates/states/home.html",
   })
 });
