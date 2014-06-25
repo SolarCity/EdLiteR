ef90a48567f048120dca542ad601e484408ac0a8
Jesse F. LaRue <lazaruslarue@gmail.com>
2014-06-25 11:25:45 -0700

    fixup! include code for d3 dependency injection

--- www/js/directives/edlD3.js
+++ www/js/directives/edlD3.js
@@ -1,25 +1,21 @@
-directives.directive('edlSvg', [ 'd3', function(d3) {
+directives.directive('edlD3', [ 'd3', function(d3) {
   return {
     restrict: "EA",
-    // templateUrl: "../../templates/directives/edlSvg.html",
-    template: '<p>hi</p>',
     link: function (scope, ele, attrs) {
-        var svg = d3.select(ele[0])
-            .append('svg')
-            .style('width', '100%');
-        console.log('hi')
-          // Browser onresize event
-          window.onresize = function() {
-            scope.$apply();
-          };
+      var svg = d3.select(ele[0])
+          .append('svg')
+          .style('width', '100%');
 
-          // hard-code data
-          scope.data = [
-            {name: "Greg", score: 98},
-            {name: "Ari", score: 96},
-            {name: 'Q', score: 75},
-            {name: "Loser", score: 48}
-          ];
+      svg.on("click", function() {
+        console.log('click');
+      })
+      // hard-code data
+      scope.data = [
+        {name: "Greg", score: 98},
+        {name: "Ari", score: 96},
+        {name: 'Q', score: 75},
+        {name: "Loser", score: 48}
+      ];
     }
   };
 }]);
