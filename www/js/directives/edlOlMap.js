fc56445035832a5d7d76cf42a989f8341bd43617
Jesse LaRue <lazaruslarue@gmail.com>
2014-08-18 15:13:55 -0700

    update -- include buttons and rudimentary button style

--- www/js/directives/edlOlMap.js
+++ www/js/directives/edlOlMap.js
@@ -1,4 +1,4 @@
-function edlOlMap($state, MapService, OlService) {
+function edlOlMap($state, MapService, OlService, StyleService) {
   return {
     restrict: "A",
     transclude: true,
@@ -8,27 +8,75 @@ function edlOlMap($state, MapService, OlService) {
     // controller: function edlOlMapCtrl($scope, $element, $attrs) {
     // },
     link: function edlOlMapLink(scope, ele, attrs) {
+      var olMapDiv = ele[0];
+
+      var pixelProjection = new ol.proj.Projection({
+        code: 'pixel',
+        units: 'pixels',
+        global: false,
+        extent: [0, 0, 960, 557] //HACK: Should be dynamic
+      });
+
+      var view =  MapService.setOview( 
+        new ol.View({
+          projection: pixelProjection,
+          center: ol.extent.getCenter(pixelProjection.getExtent()),
+          zoom: 2
+        })
+      );
+
+      // TODO: why can't angular elements be used here?
+      // make a controls box
+      var controllerbox = angular.element('<div></div>');
+      controllerbox.addClass('mapboxcontrols');
+      controllerbox.attr('id', 'edl-control-box');
+
+      // mountdraw button
+      var mountdrawbutton = angular.element('<button></button>');
+      mountdrawbutton.addClass('button button-stable topbutton');
+      mountdrawbutton.text('Mount');
+      var mountcontrol = new ol.control.Control({
+        element: mountdrawbutton[0]
+      });
+      // obstruction button
+      var obstructionbutton = angular.element('<button></button>');
+      obstructionbutton.addClass('button button-stable bottombutton');
+      obstructionbutton.text('Obstacle');
+      var obstructioncontrol = new ol.control.Control({
+        element: obstructionbutton[0],
+      });
+      // add buttons to box
+
+      controllerbox.append(mountdrawbutton);
+      controllerbox.append(obstructionbutton);
+
+      var leftsidecontrolbox = new ol.control.Control({element: controllerbox[0]});
+
+      // draw mount
+        // control function
+      var mountDraw = function mountDraw(){
+        console.log('hello mount');
+
+      };
+      var obstructionDraw = function obstructionDraw(){
+        console.log('hello obstruction');
+
+      };
+      // add control to button
+      // mountdrawbutton.on('touchstart', mountDraw, false);
+      // draw obstruction
+        // control function
+        // add control to button
+
+      mountdrawbutton.on('click', mountDraw);
+      obstructionbutton.on('click', obstructionDraw);
 
       MapService.getStatic()
       .then(init);
       function init (imgUrl) {
         // set the ol view
-        var pixelProjection = new ol.proj.Projection({
-          code: 'pixel',
-          units: 'pixels',
-          global: false,
-          extent: [0, 0, 960, 557] //HACK: Should be dynamic
-        });
 
-        var view =  MapService.setOview( 
-          new ol.View({
-            projection: pixelProjection,
-            center: ol.extent.getCenter(pixelProjection.getExtent()),
-            zoom: 2
-          })
-        );
-
-        // where we will display fixed mounting planes
+        // the picture we'll display our drawn features on
         var mapCapture = new ol.layer.Image({
           source: new ol.source.ImageStatic({
             url: imgUrl,
@@ -39,23 +87,23 @@ function edlOlMap($state, MapService, OlService) {
         });
         mapCapture.set('name', 'mapCapture');
 
-        var olMapDiv = ele[0];
-        // var olMapDiv = document.getElementById('omap');
-
-
-
-        // let's tinker with the layers
-        // get the layer... 
-        var mountPlaneImage = OlService.mountPlaneImage;
-        var mountAndGutterSource = OlService.mountAndGutterSource;
+        var mounts = OlService.mounts;
         var mountLayer = new ol.layer.Image({
-          source: mountPlaneImage
+          source: OlService.mountPlaneImage, 
+          style:  StyleService.defaultStyleFunction,
         });
         mountLayer.set('name', 'mountLayer');
 
         // add the layer to the map
+
         var mapOptions = {
           layers: [mapCapture, mountLayer],
+          controls: ol.control.defaults({
+              attributionOptions: ({
+                collapsible: false
+              })
+            // }).extend([mountcontrol, obstructioncontrol]),
+            }).extend([leftsidecontrolbox]),
           interactions: ol.interaction.defaults({
             altShiftDragRotate: true,
             dragPan: false,
@@ -66,86 +114,33 @@ function edlOlMap($state, MapService, OlService) {
         };
         var map = MapService.setOmap(mapOptions);
 
+        var select = new ol.interaction.Select({
+          features: mounts.getFeatures(),
+          condition: ol.events.condition.targetNotEditable,
+          styleFunction: StyleService.highlightStyleFunction,
+        });
+        map.addInteraction(select);
 
         var selectedMountOverlay = OlService.selectedMountOverlay;
         MapService.addOverlay(selectedMountOverlay);
 
-        //TODO: combine with selectedMountOverlay and give it a style function (i guess)
-        var gutterOverlay = OlService.gutterOverlay;
-        MapService.addOverlay(gutterOverlay);
-
-        // TODO: obstructions should work
-        // var obstructionOverlay = OlService.obstructionOverlay;
-        // MapService.addOverlay(obstructionOverlay);
-
         var modify = new ol.interaction.Modify({
-          features: selectedMountOverlay.getFeatures()          
+          features: select.getFeatures(),
+          style: StyleService.highlightStyleFunction,
         });
         map.addInteraction(modify);
-
         // // TODO: directive
         var draw = new ol.interaction.Draw({
-          features: selectedMountOverlay.getFeatures(),
+          features: mounts.getFeatures(),
           snapTolerance: 25,
           type: 'Polygon', 
           geometryName: 'mount',
+          style: StyleService.defaultStyleFunction,
         });
 
         map.addInteraction(draw);
 
-        var gutterLineFinder = function gutterLineFinder (event) {
-          var feature = event.feature;
-          console.log('feat')
-          var wkt = new ol.format.WKT();
-
-         /* 
-          *
-          * get & split the WKT (well-known text) for our feature
-          * looks like this -> POLYGON((0.7031250000000142
-          *                     7.306665009118518,27.949218750000007
-          *                     25.59079413562536,28.828124999999996
-          *                     -15.846384918461212,0.7031250000000142
-          *                   7.306665009118518))
-          *
-          */
-          var featureWkt = wkt.writeFeature(feature).split(' ');
-
-          // create a LineString to mark our gutter
-          // looks like this --> "LINESTRING(549.609375", "360.140625,372.294921875", "254.798828125)"
-          var gutterLineWkt = [
-            'LINESTRING(',
-              featureWkt[0].split('((')[1],
-              featureWkt[1],
-              featureWkt[2].split(',')[0],
-              ')'
-          ].join(' ');
-
-          // make a gutter feature to draw & push to gutterOverlay's feature collection
-          var gutterLineGeom = wkt.readGeometry(gutterLineWkt);
-          var gutterLine = new ol.Feature(gutterLineGeom);
-          gutterLine.setGeometryName('gutter');
-          gutterOverlay.addFeature(gutterLine);
-          // mountAndGutterSource.addFeature(feature.clone());
-
-          // var drawnfeature = selectedMountOverlay.getFeatures().pop(); //TODO: after feature becomes "fixed"
-          var drawnfeature = feature;
-          // var drawnfeature = selectedMountOverlay.getFeatures().getArray()[0].pop();
-
-          OlService.setRecent(feature);
-          // selectedMountOverlay.addFeature(drawnfeature.clone())
-          drawnfeature.setProperties(OlService.mountplane);
-
-          var featurearray = [gutterLine, drawnfeature];
-          var featuresindex = mountAndGutterSource.getFeatures().length;
-          mountAndGutterSource.addFeature(drawnfeature);
-
-          OlService.setIdsOfFeaturearray(featurearray, featuresindex ); 
-          $state.go('plan.mount', {id: featuresindex});
-
-        };
-                  // NEW BELOW HERE: 
-
-
+        var gutterLineFinder = OlService.gutterLineFinder;
         draw.on('drawend', gutterLineFinder);
 
       }
