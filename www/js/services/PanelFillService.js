516b8393ec40ff6a9cb25a4d8c5c5d2378a6e369
Jesse LaRue <lazaruslarue@gmail.com>
2014-09-24 19:27:23 -0700

    fixup! refactor - cleaning panelfillservice

--- www/js/services/PanelFillService.js
+++ www/js/services/PanelFillService.js
@@ -10,14 +10,14 @@ function PanelFillService_ ($q, $window, OlService, MapService, ApiService) {
    */
 
   PanelFillService.addPanelsFromApi = function(data, panelid){
-	
+		var mapEdges = edgesAndRatios();
 	  var responseIterator = function(arrayOfPanels, key){
 	    // iterate over each panel in the array of panels
 	  	var featurestoadd = [];
 
 	    arrayOfPanels.forEach(function(points_for_panel, key, obj){
 	      // turn each array of points into a WKT
-	      var feature = PanelFillService.panelFromJson(points_for_panel);
+	      var feature = PanelFillService.panelFromJson(points_for_panel, mapEdges);
 	      featurestoadd.push(feature);
 	    	OlService.setIdsOfFeaturearray([feature], panelid);
 	    });
@@ -102,7 +102,7 @@ function PanelFillService_ ($q, $window, OlService, MapService, ApiService) {
 		var px_per_e = pixelWidth  / e_mapsize;
 		var px_per_n = pixelHeight / n_mapsize;
 		var px_per_en = [px_per_e, px_per_n];
-		console.log('attempt');
+
 		return {
 
 			n: north_edge,
@@ -111,6 +111,7 @@ function PanelFillService_ ($q, $window, OlService, MapService, ApiService) {
 			w: west_edge,
 			px_per_e: px_per_e,
 			px_per_n: px_per_n,
+			pxHeight: pixelHeight,
 		};
 
   }
@@ -118,17 +119,17 @@ function PanelFillService_ ($q, $window, OlService, MapService, ApiService) {
 	PanelFillService.pointToLatLng = function(point_string) {
 		//TODO: is it a problem that browser zoom impacts these numbers?
 
-		var edges = edgesAndRatios();
+		var mapEdges = edgesAndRatios();
 
 		var pt_xy	= point_string.split(' ');
 
 
 		function north_pt(pt_y) {
-			return edges.s + pt_y / edges.px_per_n;
+			return mapEdges.s + pt_y / mapEdges.px_per_n;
 		}
 
 		function east_pt(pt_x) {
-			return edges.w + pt_x / edges.px_per_e;
+			return mapEdges.w + pt_x / mapEdges.px_per_e;
 		}
 
 		function point_machine(pt_xy){
@@ -139,53 +140,17 @@ function PanelFillService_ ($q, $window, OlService, MapService, ApiService) {
   };
 
   // turn json into WKT ==> POLYGON((159 569,541 576,554 286,193 271,159 569)) 
-	PanelFillService.panelFromJson = function(array_of_points){
+	PanelFillService.panelFromJson = function(array_of_points, mapEdges){
 		var feature_to_return;
 		var pts = array_of_points;
-		var north_edge, south_edge, east_edge, west_edge, pixelHeight, pixelWidth;
-		north_edge = 0;
-		west_edge  = 0;
-		east_edge  = $window.innerWidth;
-		south_edge = OlService.mapDiv.clientHeight;
-		pixelWidth = $window.innerWidth;
-		pixelHeight = OlService.mapDiv.clientHeight;
-
-		var mapCornerExtent;
-		mapCornerExtent = MapService.g.bounds ? MapService.g.bounds : [0, 0, $window.innerWidth, OlService.mapDiv.clientHeight ];
-		var pixelExtentXY01 = [0, 0, $window.innerWidth, OlService.mapDiv.clientHeight ];
-		var enws_deg_extent = [];
-		if (!Array.isArray(mapCornerExtent)) { //TODO: ?? extract this to MapService
-			north_edge = mapCornerExtent.getNorthEast().lat();
-			south_edge = mapCornerExtent.getSouthWest().lat();
-			east_edge  = mapCornerExtent.getNorthEast().lng();
-			west_edge  = mapCornerExtent.getSouthWest().lng();
-			enws_deg_extent = [
-				mapCornerExtent.getNorthEast().lng(), // degX0 east_edge
-				mapCornerExtent.getNorthEast().lat(), // degY0 north_edge
-				mapCornerExtent.getSouthWest().lng(), // degX1 west_edge
-				mapCornerExtent.getSouthWest().lat(), // degY1 south_edge
-			];
-		} else {
-			enws_deg_extent = mapCornerExtent;
-		}
-
-		// map measure in degrees
-		var e_mapsize = Math.abs(east_edge - west_edge);
-		var n_mapsize = Math.abs(north_edge - south_edge);
-		var en_mapsize = [e_mapsize, n_mapsize];
-
-		// ratio pixels on map : degrees on map = px/dg
-		var px_per_e = pixelWidth  / e_mapsize;
-		var px_per_n = pixelHeight / n_mapsize;
-		var px_per_en = [px_per_e, px_per_n];
 
 		function pointJoin(pointCoordLngLat){
 			var pt_e  = pointCoordLngLat[0]; // -122.26724295911637
 			var pt_n = pointCoordLngLat[1]; //   37.483464075107776			
 			// (en_pt_on_map) * K = new point
 			var result = [
-				 (pt_e - west_edge ) * px_per_e ,
-			   pixelHeight - ((north_edge - pt_n) * px_per_n) ,
+				 (pt_e - mapEdges.w ) * mapEdges.px_per_e ,
+			   mapEdges.pxHeight - ((mapEdges.n - pt_n) * mapEdges.px_per_n) ,
 			].join(' ');
 
 			return result;
