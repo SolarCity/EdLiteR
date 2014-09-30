0c3cb2d68ae35f1aea8cbeb9f79c065ae7fff6cc
Jesse LaRue <lazaruslarue@gmail.com>
2014-09-30 11:48:58 -0700

    repairing git mistake from yesterday

--- www/js/controllers/plan/PlanCtrl.js
+++ www/js/controllers/plan/PlanCtrl.js
@@ -44,47 +44,7 @@ function PlanCtrl_($scope, $rootScope, $timeout, $ionicSideMenuDelegate, Feature
       return edl;
     }
     return feature.get('edl');
-
   };
 
-  vm.featureCorners = function() {
-    var mountPoints = {}; // we'll send this to the api
-    var wkt = OlService.wkt; // used for turning features to strings
-    //set zoom to initial zoomlevel // HACK: avoid using projection pixel <> latlng     
-    MapService.getView().setZoom(OlService.defaultZoom);
-    // get features
-    var mounts  = OlService.mounts.getFeatures();
-    mountPoints.m = {};
-    var obstructions = OlService.obstructions.getFeatures();
-    console.log(obstructions);
-    mountPoints.o = {};
-    // for features by type "mount" 
-    mounts.forEach(function(feat, idx, col){
-      if (feat.getGeometryName() === "mount") {
-        idx = parseInt(idx);
-        // add their points to mountpoints
-        mountPoints.m[idx] = wkt.writeFeature(feat).split(',');
-        mountPoints.m[idx][0] = mountPoints.m[idx][0].split('((')[1];
-        mountPoints.m[idx].splice(-1); // remove the last point, it's a dupe of the 1st
-        // mountPoints.m[idx].feat = feat;
-      }
-    });
-    obstructions.forEach(function(feat, idx, col){
-      if (feat.getGeometryName() === "obstruction") {
-        idx = parseInt(idx);
-        // add their points to mountpoints
-        mountPoints.o[idx] = wkt.writeFeature(feat).split(',');
-        mountPoints.o[idx][0] = mountPoints.o[idx][0].split('(')[1];
-        mountPoints.o[idx][0] = mountPoints.o[idx][0].split(')');
-        mountPoints.o[idx].splice(1);
-        mountPoints.o[idx] = mountPoints.o[idx][0];
-        // mountPoints.o[idx].feat = feat;
-      }
-    });
-    // vm.buildMessage(mountPoints, {});
-    vm.apiMessage = PanelFillService.processFeatures(mountPoints.m, mountPoints.o);
-    $scope.apiMessage = vm.apiMessage;
-  };
-  
 }
 controllers.controller('PlanCtrl',PlanCtrl_);
