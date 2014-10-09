40492d97967cf5f067a68e8801efa8e52cc5d0e1
Jesse LaRue <lazaruslarue@gmail.com>
2014-10-09 13:40:15 -0700

    cleanup - corrected spacing

--- www/js/detailmenu/DetailCtrl.js
+++ www/js/detailmenu/DetailCtrl.js
@@ -1,26 +1,32 @@
-function DetailCtrl_($ionicSideMenuDelegate, OlService, ApiService, PanelFillService) {
-	var vm = this;
-	var Ol = OlService;
+function DetailCtrl_($ionicSideMenuDelegate, OlService, MapService, ApiService, PanelFillService) {
+  var vm = this;
+  var Ol = OlService;
 
   vm.fillPanel = function fillPanel(feature) {
-	  if (feature !== null) {
-	      // if selected feature has panels, delete them
-	      var id = feature.getId();
-	      var panellayer = Ol.layers.panel;
-	      var existing = panellayer.getFeatureById(id);
-	      if (existing) {
-	          Ol.removeFeatureById(id, panellayer);
-	      }
-	      var msg = Ol.fillMessageForSingleMount(feature);
-	      // create api message with Process Features
-	      var api = PanelFillService.processFeatures(msg.m, msg.o);
-	      $ionicSideMenuDelegate.toggleRight();
-	      ApiService.uploadMounts(api) //TODO: change from sample
-	        .then(function (data) {
-	          PanelFillService.addPanelsFromApi(data, id);
-	      });
-	  }
-	};
+    var panelstoadd;
+    if (feature !== null) {
+        // if selected feature has panels, delete them
+        var id = feature.getId();
+        var panellayer = Ol.layers.panel;
+        var existing = panellayer.getFeatureById(id);
+        if (existing) {
+            Ol.removeFeatureById(id, panellayer);
+        }
+        var msg = Ol.fillMessageForSingleMount(feature);
+        // create api message with Process Features
+        var api = PanelFillService.processFeatures(msg.m, msg.o);
+        $ionicSideMenuDelegate.toggleRight();
+        ApiService.uploadMounts(api) //TODO: change from sample
+          .then(function (data) {
+            panelstoadd = PanelFillService.makePanelsWithApiResponse(data, id);
+            OlService.panels.addFeatures(panelstoadd);
+            vm.mountPlanePopup(feature, panelstoadd);
+        });
+
+
+    }
+  };
 }
 
 controllers.controller("DetailCtrl", DetailCtrl_);
+
